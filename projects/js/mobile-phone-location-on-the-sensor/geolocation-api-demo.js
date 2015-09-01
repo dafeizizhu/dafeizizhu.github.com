365a77b067e44d82d313a154cb733ca967a264dd
maizhiying <zhiying.mai@gmail.com>
2015-09-01 17:51:25 +0800

    为Geolocation的Demo增加百度地图

--- projects/js/mobile-phone-location-on-the-sensor/geolocation-api-demo.js
+++ projects/js/mobile-phone-location-on-the-sensor/geolocation-api-demo.js
@@ -1,30 +1,42 @@
 $(function () {
+  function success(position) {
+    var str = '您当前的位置，纬度：' + position.coords.latitude + '，经度：' + position.coords.longitude
+    $('#geoloc').html(str)
+
+    // draw a google map
+    var $mapCanvas = $('#map_canvas')
+    $mapCanvas.height(400)
+    $mapCanvas.width(560)
+
+    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+    var options = {
+      zoom: 15,
+      center: latlng,
+      mapTypeControl: false,
+      navigationControlOptions: {
+        style: google.maps.NavigationControlStyle.SMALL
+      },
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map($('#map_canvas')[0], options)
+    var marker = new google.maps.Marker({
+      position: latlng,
+      map: map,
+      title: '您在这里！'
+    })
+
+    var baiduMap = new BMap.Map('baidu_map_canvas')
+    baiduMap.centerAndZoom(new BMap.Point(position.coords.longitude, position.coords.latitude), 11)
+    baiduMap.addControl(new BMap.MapTypeControl())
+    baiduMap.setCurrentCity('广州')
+    baiduMap.enableScrollWheelZoom(true)
+    var baiduMarker = new BMap.Marker(new BMap.Point(position.coords.longitude, position.coords.latitude))
+    baiduMap.addOverlay(baiduMarker)
+  }
+
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
-      var str = '您当前的位置，纬度：' + position.coords.latitude + '，经度：' + position.coords.longitude
-      $('#geoloc').html(str)
-
-      // draw a google map
-      var $mapCanvas = $('#map_canvas')
-      $mapCanvas.height(400)
-      $mapCanvas.width(560)
-
-      var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.latitude)
-      var options = {
-        zoom: 15,
-        center: latlng,
-        mapTypeControl: false,
-        navigationControlOptions: {
-          style: google.maps.NavigationControlStyle.SMALL
-        },
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
-      var map = new google.maps.Map($('#map_canvas')[0], options)
-      var marker = new google.maps.Marker({
-        position: latlng,
-        map: map,
-        title: '您在这里！'
-      })
+      success(position)
     }, function (err) {
       $('#geoloc').html(err.code + '<br/>' + err.message)
     })
